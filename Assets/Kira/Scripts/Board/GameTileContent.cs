b0dc49944046ecdeb795c2cadd0dde26d7a58fdb
Kira <kira.eva@protonmail.com>
2023-06-15 18:32:25 +0100

    Create tile prefabs

---
+++ Assets/Kira/Scripts/Board/GameTileContent.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Kira.Board
+{
+    public enum GameTileContentType
+    {
+        Empty,
+        Destination
+    }
+
+    public class GameTileContent : MonoBehaviour
+    {
+        [SerializeField]
+        private GameTileContentType type = default;
+        public GameTileContentType Type => type;
+
+        private GameTileContentFactory originFactory;
+
+        public GameTileContentFactory OriginFactory
+        {
+            get => originFactory;
+            set
+            {
+                Debug.Assert(originFactory == null, "Null Origin Factory!!");
+                originFactory = value;
+            }
+        }
+    }
+}
---
+++ Assets/Kira/Scripts/Board/GameTileContentFactory.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Kira.Board
+{
+    [CreateAssetMenu(fileName = "New TileContentFactory", menuName = "Kira/Create TileContent Factory", order = 0)]
+    public class GameTileContentFactory : ScriptableObject
+    {
+        public void Reclaim(GameTileContent content)
+        {
+            
+        }
+    }
+}
